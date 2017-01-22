3a8bb200862aa0add5c07f5ef30d23c07f67a712
madisonwalls <madilyn@live.unc.edu>
2017-01-22 13:29:39 -0500

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,40 @@
+console.info('Welcome to the dessert maker v1.0.');
+
+var currentFruit = null;
+var currentDessert = null;
+
+var images = {
+  fruits: {
+    apple: 'img/Apple.jpg',
+    orange: 'img/Orange.jpg',
+    banana: 'img/Banana.jpg'
+  },
+  desserts: {
+    icecream: 'img/IceCream.jpg',
+    cake: 'img/Cake.jpg',
+    pie: 'img/Pie.jpg'
+  },
+  mixes: {
+    apple: {
+      icecream: 'img/AppleIceCream.jpg',
+      cake: 'img/AppleCake.jpg',
+      pie: 'img/ApplePie.jpg'
+    },
+    orange: {
+      icecream: 'img/OrangeIceCream.jpg',
+      cake: 'img/OrangeCake.jpg',
+      pie: 'img/OrangePie.jpg'
+    },
+    banana: {
+      icecream: 'img/BananaIceCream.jpg',
+      cake: 'img/BananaCake.jpg',
+      pie: 'img/BananaPie.jpg'
+    }
+  }
+};
+
+var apple = document.querySelector('.Fruit--apple');
+apple.addEventListener('click', function() {
+  console.log('Apple clicked.');
+  // update image in fruit holder
+});
